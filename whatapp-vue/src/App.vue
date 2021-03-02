6b7600721a543dc8cf1d27be9e03f851dfb2ed56
Luis <luis.here.j@gmail.com>
2021-03-02 12:59:12 +0100

    Lado izquierdo y fondo

--- whatapp-vue/src/App.vue
+++ whatapp-vue/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home></Home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Home
+  },
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background: linear-gradient(180deg, #019789 0%,#019789 15%, #d9dbd5 15%, #d9dbd5 100%);
+    background-repeat: no-repeat;
 }
 </style>
---
+++ whatapp-vue/src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <div id="columnaIzquierda">
+      <div class="container">
+          <div>
+              <div>
+                  <div class="usuario">
+                        <img alt="user avatar" src="../assets/user-avatar_opt.jpg" id="userAvatar">
+                        <p>Luis</p>
+                    </div>
+                    <div>
+                        <p>Recibe las notificaciones de mensajes nuevos</p>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="Buscar o empezar un nuevo chat">
+                    </div>
+                    <div>
+                        <img alt="user avatar" src="../assets/user-avatar_opt.jpg" id="userAvatar">
+                        <p>Fulanito</p>
+                    </div>
+                    <div>
+                        <img alt="user avatar" src="../assets/user-avatar_opt.jpg" id="userAvatar">
+                        <p>P1_Coders</p>
+                    </div>
+                    <div>
+                        <img alt="user avatar" src="../assets/user-avatar_opt.jpg" id="userAvatar">
+                        <p>Menganito</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="columnaDerecha">
+      <div class="container">
+          <div>
+              <div>
+                  <div class="usuario">
+                        <img alt="user avatar" src="../assets/user-avatar_opt.jpg" id="userAvatar">
+                        <p>Luis</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
